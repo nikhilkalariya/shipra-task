aaad491c56c7ac758beecbae9ada4bcb9bfedea1
nikhilkalariya <nikhilkalariya1000@gmail.com>
2025-07-19 16:01:35 +0530

    rmove unwanted import

--- app/overview/OverviewChart.tsx
+++ app/overview/OverviewChart.tsx
@@ -14,10 +14,7 @@ import { TrendingUp } from "lucide-react";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import { fetchFromAPI } from "../utils/api";
 
